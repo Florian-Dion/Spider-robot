f8961aad318be2771def287fc2d9f80515e9f91c
florian <floflo.dion@free.fr>
2025-01-20 20:38:11 +0100

    Modif driver usart

---
+++ STM32RT/include/stm32f4/src/usart.c
@@ -0,0 +1,9 @@
+#include "usart.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void stm32f4_uart1_init(void){
+    
+    USART1->BRR.
+}
--- STM32RT/include/stm32f4/usart.h
+++ STM32RT/include/stm32f4/usart.h
@@ -1,3 +1,23 @@
+/**
+ * Torpy - grid-follower wheeling and box delivering bot.
+ *	Copyright (C) 2021  Université de Toulouse <floriandion8@gmail.com>
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ */
+
+/**
+ * @file usart.h
+ * @brief Universal synchronous asynchronous receiver transmitter (USART) HW driver header file.
+ */
+
 #ifndef STM32F4_USART_H
 #define STM32F4_USART_H
 
@@ -13,8 +33,15 @@
 #define USART1_CR3 USART1_REG(0x14)
 #define USART1_GTPR USART1_REG(0x18)
 
-#define USART1_SR USART1_REG(0x00)
 
+/** USART Return Code */
+#define USART_ERROR -1
+
+/**********************************************************************//**
+ * @name IO Device: Universal synchronous asynchronous receiver transmitter (USART)
+ **************************************************************************/
+/**@{*/
+/** USART SR Register prototype */
 typedef volatile struct {
     uint32_t PE       : 1;  // Bit 0: Parity error
     uint32_t FE       : 1;  // Bit 1: Framing error
@@ -29,17 +56,21 @@ typedef volatile struct {
     uint32_t Reserved : 22; // Bits 10–31: Reserved
 } USART_SR_t;
 
+
+/** USART DR Register prototype */
 typedef volatile struct {
     uint32_t DR       : 9;  // Bits 0–8: Data Register
     uint32_t Reserved : 23; // Bits 9–31: Reserved
 } USART_DR_t;
 
+/** USART BRR Register prototype */
 typedef volatile struct {
     uint32_t DIV_Fraction : 4;  // Bits 0–3: Fraction of USART Divider
     uint32_t DIV_Mantissa : 12; // Bits 4–15: Mantissa of USART Divider
     uint32_t Reserved     : 16; // Bits 16–31: Reserved
 } USART_BRR_t;
 
+/** USART CR1 Register prototype */
 typedef volatile struct {
     uint32_t SBK      : 1;  // Bit 0: Send Break
     uint32_t RWU      : 1;  // Bit 1: Receiver Wakeup
@@ -55,10 +86,13 @@ typedef volatile struct {
     uint32_t WAKE     : 1;  // Bit 11: Wakeup method
     uint32_t M        : 1;  // Bit 12: Word length
     uint32_t UE       : 1;  // Bit 13: USART Enable
+    uint32_t Reserved : 1; // Bits 16–31: Reserved
     uint32_t OVER8    : 1;  // Bit 15: Oversampling mode
-    uint32_t Reserved : 17; // Bits 16–31: Reserved
+    uint32_t Reserved2 : 16; // Bits 16–31: Reserved
 } USART_CR1_t;
 
+
+/** USART CR2 Register prototype */
 typedef volatile struct {
     uint32_t ADD      : 4;  // Bits 0–3: Address of the USART node
     uint32_t Reserved : 1;  // Bit 4: Reserved
@@ -74,6 +108,8 @@ typedef volatile struct {
     uint32_t Reserved3: 17; // Bits 15–31: Reserved
 } USART_CR2_t;
 
+
+/** USART CR3 Register prototype */
 typedef volatile struct {
     uint32_t EIE      : 1;  // Bit 0: Error Interrupt Enable
     uint32_t IREN     : 1;  // Bit 1: IrDA mode Enable
@@ -90,12 +126,16 @@ typedef volatile struct {
     uint32_t Reserved : 20; // Bits 12–31: Reserved
 } USART_CR3_t;
 
+
+/** USART GTPR Register prototype */
 typedef volatile struct {
     uint32_t PSC      : 8;  // Bits 0–7: Prescaler value
     uint32_t GT       : 8;  // Bits 8–15: Guard time value
     uint32_t Reserved : 16; // Bits 16–31: Reserved
 } USART_GTPR_t;
 
+
+/** USART Register prototype */
 typedef volatile struct {
     USART_SR_t   SR;   // Offset 0x00: Status Register
     USART_DR_t   DR;   // Offset 0x04: Data Register
@@ -106,6 +146,13 @@ typedef volatile struct {
     USART_GTPR_t GTPR; // Offset 0x18: Guard Time and Prescaler Register
 } USART_t;
 
-#define UART1 ((USART_t*) (USART1_BASE))
+/** USART1 module hardware access (#USART_t) */
+#define USART1 ((USART_t*) (USART1_BASE))
+
+/**********************************************************************//**
+ * @name Prototypes
+ **************************************************************************/
+/**@{*/
+int stm32f4_uart1_init(void);
 
 #endif //STM32F4_USART_H
