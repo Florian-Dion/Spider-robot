798d28d00863708421e3729419647ef73de8a195
Florian-Dion <floflo.dion@free.fr>
2025-02-18 18:01:55 +0100

    3 pattes tenues

--- STM32RT/include/stm32f4/src/init.c
+++ STM32RT/include/stm32f4/src/init.c
@@ -19,7 +19,8 @@
 #define DELAY_20MS (60000-1)
 /********************************************************************************************************** */
 
-void set_servo1B(int n){TIM2_CCR1 = SERVO_05MS + (n*(SERVO_1MS/900));} //good
+void set_servo1B(int n){TIM8_CCR1 = SERVO_05MS + (n*(SERVO_1MS/900));} //good
+
 void set_servo3A(int n){TIM2_CCR2 = SERVO_05MS + (n*(SERVO_1MS/900));} //good
 void set_servo4A(int n){TIM2_CCR3 = SERVO_05MS + (n*(SERVO_1MS/900));} //good
 void set_servo4C(int n){TIM2_CCR4 = SERVO_05MS + (n*(SERVO_1MS/900));} //good
@@ -75,60 +76,61 @@ void set_servo6C(int n){TIM2_CCR3 = SERVO_05MS + (n*(SERVO_1MS/900));}*/
 }*/
 
 
-// TIM3 init on GPIOB A15, PB3, PB10, PB11
+// TIM3 init on GPIOB PB3, PB10, PB11
 void init_TIM2(){
-    GPIOA_MODER = REP_BITS(GPIOB_MODER, 15*2, 2, GPIO_MODER_ALT);
     GPIOB_MODER = REP_BITS(GPIOB_MODER, 3*2, 2, GPIO_MODER_ALT);
     GPIOB_MODER = REP_BITS(GPIOB_MODER, 10*2, 2, GPIO_MODER_ALT);
     GPIOB_MODER = REP_BITS(GPIOB_MODER, 11*2, 2, GPIO_MODER_ALT);
-    GPIOA_AFRH = REP_BITS(GPIOA_AFRH, (15-8) * 4, 4, 1);
     GPIOB_AFRL = REP_BITS(GPIOB_AFRL, 3 * 4, 4, 1);
     GPIOB_AFRH = REP_BITS(GPIOB_AFRH, (10-8) * 4, 4, 1);
     GPIOB_AFRH = REP_BITS(GPIOB_AFRH, (11-8) * 4, 4, 1);
 
     //TIM3 init
-    TIM2_CCMR1 = TIM_OC1M_PWM1 | TIM_OC2M_PWM1;
+    TIM2_CCMR1 = TIM_OC2M_PWM1;
     TIM2_CCMR2 = TIM_OC3M_PWM1 | TIM_OC4M_PWM1;
-    TIM2_CCER = TIM_CC1E | TIM_CC2E | TIM_CC3E | TIM_CC4E;
+    TIM2_CCER = TIM_CC2E | TIM_CC3E | TIM_CC4E;
+
     TIM2_CR1 = 0;
     TIM2_PSC = SERVO_PSC;
     TIM2_ARR = SERVO_PERIOD;
     TIM2_EGR = TIM_UG;
     TIM2_SR = 0;
-    set_servo1B(900);
-    set_servo3A(900);
-    set_servo4A(900);
-    set_servo4C(900);
+    set_servo3A(1800);
+    set_servo4A(200);
+    set_servo4C(1200);
     TIM2_CR1 = TIM_CEN | TIM_ARPE;
 
     printf("TIM2 init done\n");
 }
 
 
-// TIM3 init on GPIOB PB4, PB5, PB0, PB1
+// TIM3 init on GPIOB PB4, PB5, PC8, PB1
 void init_TIM3(){
     GPIOB_MODER = REP_BITS(GPIOB_MODER, 4*2, 2, GPIO_MODER_ALT);
     GPIOB_MODER = REP_BITS(GPIOB_MODER, 5*2, 2, GPIO_MODER_ALT);
-    GPIOB_MODER = REP_BITS(GPIOB_MODER, 0*2, 2, GPIO_MODER_ALT);
+    GPIOC_MODER = REP_BITS(GPIOC_MODER, 8*2, 2, GPIO_MODER_ALT);
     GPIOB_MODER = REP_BITS(GPIOB_MODER, 1*2, 2, GPIO_MODER_ALT);
     GPIOB_AFRL = REP_BITS(GPIOB_AFRL, 4 * 4, 4, 2);
     GPIOB_AFRL = REP_BITS(GPIOB_AFRL, 5 * 4, 4, 2);
-    GPIOB_AFRL = REP_BITS(GPIOB_AFRL, 0 * 4, 4, 2);
+    GPIOC_AFRH = REP_BITS(GPIOC_AFRH, (8-8) * 4, 4, 2);
     GPIOB_AFRL = REP_BITS(GPIOB_AFRL, 1 * 4, 4, 2);
 
     //TIM3 init
     TIM3_CCMR1 = TIM_OC1M_PWM1 | TIM_OC2M_PWM1;
     TIM3_CCMR2 = TIM_OC3M_PWM1 | TIM_OC4M_PWM1;
+    //TIM3_CCMR1 = TIM_OC1M_PWM1 | TIM_OC2M_PWM1;
+    //TIM3_CCMR2 = TIM_OC3M_PWM1 | TIM_OC4M_PWM1;
     TIM3_CCER = TIM_CC1E | TIM_CC2E | TIM_CC3E | TIM_CC4E;
+    //TIM3_CCER = TIM_CC1E | TIM_CC3E | TIM_CC4E;
     TIM3_CR1 = 0;
     TIM3_PSC = SERVO_PSC;
     TIM3_ARR = SERVO_PERIOD;
     TIM3_EGR = TIM_UG;
     TIM3_SR = 0;
     set_servo3C(900);
-    set_servo3B(900);
+    set_servo3B(1100);
     set_servo1A(900);
-    set_servo4B(900);
+    set_servo4B(1100);
     TIM3_CR1 = TIM_CEN | TIM_ARPE;
 
     printf("TIM3 init done\n");
@@ -148,16 +150,19 @@ void init_TIM4(){
     //TIM4 init
     TIM4_CCMR1 = TIM_OC1M_PWM1 | TIM_OC2M_PWM1;
     TIM4_CCMR2 = TIM_OC3M_PWM1 | TIM_OC4M_PWM1;
+    //TIM4_CCMR1 = TIM_OC1M_PWM1 | TIM_OC2M_PWM1;
+    //TIM4_CCMR2 = TIM_OC3M_PWM1;
     TIM4_CCER = TIM_CC1E | TIM_CC2E | TIM_CC3E | TIM_CC4E;
+    //TIM4_CCER = TIM_CC3E;
     TIM4_CR1 = 0;
     TIM4_PSC = SERVO_PSC;
     TIM4_ARR = SERVO_PERIOD;
     TIM4_EGR = TIM_UG;
     TIM4_SR = 0;
-    set_servo2A(900);
-    set_servo2C(900);
+    set_servo2A(1200);
+    set_servo2C(400);
     set_servo1C(900);
-    set_servo2B(900);
+    set_servo2B(1600);
     TIM4_CR1 = TIM_CEN | TIM_ARPE;
 
     printf("TIM4 init done\n");
@@ -199,32 +204,21 @@ void init_TIM4(){
 }*/
 
 // TIM8 init on GPIOC PC6, PC7, PC8, PC9
-/*void init_TIM8(){
+void init_TIM8(){
     GPIOC_MODER = REP_BITS(GPIOC_MODER, 6*2, 2, GPIO_MODER_ALT);
-    GPIOC_MODER = REP_BITS(GPIOC_MODER, 7*2, 2, GPIO_MODER_ALT);
-    GPIOC_MODER = REP_BITS(GPIOC_MODER, 8*2, 2, GPIO_MODER_ALT);
-    GPIOC_MODER = REP_BITS(GPIOC_MODER, 9*2, 2, GPIO_MODER_ALT);
     GPIOC_AFRL = REP_BITS(GPIOC_AFRL, 6 * 4, 4, 3);
-    GPIOC_AFRL = REP_BITS(GPIOC_AFRL, 7 * 4, 4, 3);
-    GPIOC_AFRH = REP_BITS(GPIOC_AFRH, (8-8) * 4, 4, 3);
-    GPIOC_AFRH = REP_BITS(GPIOC_AFRH, (9-8) * 4, 4, 3);
 
     TIM8_CR1 = 0;
     TIM8_PSC = SERVO_PSC * 2;
     TIM8_ARR = SERVO_PERIOD;
-    TIM8_CCMR1 |= TIM_OC1M_PWM1 | TIM_OC2M_PWM1;
-    TIM8_CCMR2 |= TIM_OC3M_PWM1 | TIM_OC4M_PWM1;
-    TIM8_CCER |= TIM_CC1E | TIM_CC2E | TIM_CC3E | TIM_CC4E;
+    TIM8_CCMR1 |= TIM_OC1M_PWM1;
+    TIM8_CCER |= TIM_CC1E;
     TIM8_BDTR |= TIM1_MOE;
-    //set_servo6C(2*900);
-    // TIM8_CCR1 = SERVO_1MS;
-    // TIM8_CCR2 = SERVO_1MS;
-    // TIM8_CCR3 = SERVO_1MS;
-    // TIM8_CCR4 = SERVO_1MS;
+    set_servo1B(100);
     TIM8_CR1 |= TIM_CEN;
 
     printf("TIM8 init done\n");
-}*/
+}
 
 void init_servo(){
 
@@ -256,8 +250,7 @@ void init_servo(){
     init_TIM3();
     init_TIM2();
     init_TIM4();
-    //init_TIM5();
-    //init_TIM8();
+    init_TIM8();
 
     /**
      * Un timer qui va générer une interruption toutes les n ms pour update les servomoteurs
